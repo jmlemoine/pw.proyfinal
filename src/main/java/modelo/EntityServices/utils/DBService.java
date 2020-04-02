a2e04dbfcbd123f6825b9724c8e1ba8f42cd013d
RSDarcy <39503717+RSDarcy@users.noreply.github.com>
2020-04-02 18:46:25 -0400

    Update DBService

--- src/main/java/modelo/EntityServices/utils/DBService.java
+++ src/main/java/modelo/EntityServices/utils/DBService.java
@@ -2,14 +2,18 @@ package modelo.EntityServices.utils;
 
 import org.h2.tools.Server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBService {
 
     private static DBService instancia;
 
     public static DBService getInstancia() {
-        if(instancia == null){
+        if (instancia == null) {
             instancia = new DBService();
         }
         return instancia;
@@ -19,15 +23,13 @@ public class DBService {
         try {
             Server.createTcpServer("-tcpPort","9092","-tcpAllowOthers","-tcpDaemon").start();
 
-        }
-        catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Problema con la base de datos: "+ex.getMessage());
-
         }
     }
-
     public void init(){
         iniciarDn();
     }
+
 
 }
