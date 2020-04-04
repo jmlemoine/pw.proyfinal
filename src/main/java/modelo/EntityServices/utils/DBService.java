3115740b2df53f424930b664ca3c46f2ba713794
RSDarcy <39503717+RSDarcy@users.noreply.github.com>
2020-04-03 20:25:40 -0400

    Reversing DB Changes

--- src/main/java/modelo/EntityServices/utils/DBService.java
+++ src/main/java/modelo/EntityServices/utils/DBService.java
@@ -2,9 +2,8 @@ package modelo.EntityServices.utils;
 
 import org.h2.tools.Server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+<<<<<<< HEAD
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
@@ -14,14 +13,22 @@ import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaQuery;
 import java.lang.reflect.Field;
 import java.util.List;
+=======
+>>>>>>> parent of a2e04db... Update DBService
 
 public class DBService {
 
     private static DBService instance;
 
+<<<<<<< HEAD
     public static DBService getInstance() {
         if (instance == null) {
             instance = new DBService();
+=======
+    public static DBService getInstancia() {
+        if(instancia == null){
+            instancia = new DBService();
+>>>>>>> parent of a2e04db... Update DBService
         }
         return instance;
     }
@@ -29,13 +36,15 @@ public class DBService {
         try {
             Server.createTcpServer("-tcpPort","9092","-tcpAllowOthers","-tcpDaemon").start();
 
-        } catch (SQLException ex) {
+        }
+        catch (SQLException ex){
             System.out.println("Problema con la base de datos: "+ex.getMessage());
+
         }
     }
+
     public void init(){
         iniciarDn();
     }
-
 
 }
