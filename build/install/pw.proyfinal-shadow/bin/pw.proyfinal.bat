07d796391dec28bf6c3790330a82c13319571a2e
RSDarcy <39503717+RSDarcy@users.noreply.github.com>
2020-04-05 14:51:19 -0400

    Update ShadowJar

--- build/install/pw.proyfinal-shadow/bin/pw.proyfinal.bat
+++ build/install/pw.proyfinal-shadow/bin/pw.proyfinal.bat
@@ -63,7 +63,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\pw.proyfinal-1.0-SNAPSHOT-all.jar
+set CLASSPATH=%APP_HOME%\lib\pw.proyfinal-0.1-all.jar
 
 @rem Execute pw.proyfinal
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %PW_PROYFINAL_OPTS%  -jar "%CLASSPATH%" %CMD_LINE_ARGS%
